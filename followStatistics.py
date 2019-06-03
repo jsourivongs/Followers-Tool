30f83a65f104dc385d5d22778cd76dd00c0445ef
Julian Sourivongs <jsourivongs@gmail.com>
2019-06-02 21:22:00 -0700

    removed unused stuff from main

--- followStatistics.py
+++ followStatistics.py
@@ -11,35 +11,47 @@ class FollowStatistics:
     doesntFollowYou=dict()
 
     # Functions
-    def numFollowers():
-        return numFollowers
+    def printMutuals(self):
+        mutuals = iter(self.mutualFollowers)
+        for user in mutuals:
+            print(user)
+        return
 
-    def numFollowing():
-        return numFollowing
+    def printNoFBs(self):
+        nonFB = iter(self.doesntFollowYou)
+        for user in nonFB:
+            print(user)
+        return
 
-    def printMutuals():
-        print("Not yet implemented")
+    def printUsers(self, userDict):
+        users = iter(userDict)
+        for user in users:
+            print(user)
+        return
 
-    def initFollowing(self, path):
+    def _initFollowing(self, path):
         f = open(path)
         soup = BeautifulSoup(f, features="html.parser")
         following = soup.find_all("a", class_="FPmhX")
         for u in following:
             user = u.string
             self.following[user]=True
+            self.numFollowing += 1
+        return
 
-    def initFollowers(self, path):
+    def _initFollowers(self, path):
         f = open(path)
         soup = BeautifulSoup(f, features="html.parser")
         following = soup.find_all("a", class_="FPmhX")
         for u in following:
             user = u.string
             self.followers[user]=True
+            self.numFollowers += 1
         return
 
     def initTheRest(self, followingPath, followersPath):
-        self.initFollowing(followingPath)
-        self.initFollowers(followersPath)
+        self._initFollowing(followingPath)
+        self._initFollowers(followersPath)
         for user in self.followers:
             if user in self.following:
                 self.mutualFollowers[user]=True
--- main.py
+++ main.py
@@ -4,54 +4,7 @@ from bs4 import BeautifulSoup
 
 #result = requests.get("https://twitter.com/JSourivongs")
 #content = result.content
-class Followstats:
-    following=False
-    followsYou=False
-    def follows_you():
-        return followsYou
-    def following():
-        return following
-
-stats = dict()
-followers = open("followers.html")
-following = open('following.html')
-
-followers_soup = BeautifulSoup(followers, features="html.parser")
-following_soup = BeautifulSoup(following, features="html.parser")
-
-followers = followers_soup.find_all("a", class_="FPmhX")
-following = following_soup.find_all("a", class_="FPmhX")
-
-for follower in followers:
-    #print(follower.string)
-    status=Followstats()
-    status.follows_you = True
-    status.following = False
-    stats[follower.string]=status
-
-for following in following:
-    #print(follower.string)
-    if following.string not in stats:
-        status=Followstats()
-        status.following = True
-        status.follows_you = False
-        stats[following.string]=status
-    else:
-        stats[following.string].following = True
-
-it = iter(stats)
-for u in it:
-    user = stats[u]
-    #if user.follows_you and user.following:
-        #print(u.string + " and you follow each other!")
-    #elif user.following:
-        #print(u + " does not follow you back!")
-    #else:
-        #print(u + " is not followed back by you!")
 
 julian = followStatistics.FollowStatistics()
 julian.initTheRest("following.html", "followers.html")
-#it = iter(julian.dontFollowBack)
-it = iter(julian.doesntFollowYou)
-for f in it:
-    print(f)
+julian.printNoFBs()
